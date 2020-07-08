043f2afb55b7f280813233f67b604f9f04f091f3
yhtseng1101 <yhtseng1101@gmail.com>
2020-07-08 17:10:16 -0500

    update button

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ getHeight();
 
 let url = [];
 
+fetch(`https://api.unsplash.com/photos/random?query=painting&count=6&client_id=i_aQLms0SMfh9pUAJLdDJP5uQIJzKCmuWmNy1qNY27w`)
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data);
+        for(var i=0; i < data.length; i++) {
+            url.push(data[i].urls.regular);
+            document.getElementsByClassName('photo')[i].src = url[i];
+            console.log(url);
+        }
+    })
+    .catch(err => alert(err))
+
+
 function getPhoto(value) {
     url = [];
     fetch(`https://api.unsplash.com/photos/random?query=${value}&count=6&client_id=i_aQLms0SMfh9pUAJLdDJP5uQIJzKCmuWmNy1qNY27w`)
@@ -19,8 +32,6 @@ function getPhoto(value) {
         for(var i=0; i < data.length; i++) {
             url.push(data[i].urls.regular);
             document.getElementsByClassName('photo')[i].src = url[i];
-            document.getElementsByClassName('loader')[i].classList.add('hide');
-            document.getElementsByClassName('photo')[i].classList.remove('hide');
             console.log(url);
         }
     })
